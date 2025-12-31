55b57d08bc54f52dd4b29c937d121d2bd7b2aeff
absales <absales91@gmail.com>
2025-12-31 16:31:29 +0530

    .

---
+++ database/migrations/2025_12_31_110056_update_attendances_employee_fk_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+             $table->dropForeign(['employee_id']);
+
+            // ✅ Add new foreign key (users table)
+            $table->foreign('employee_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            //
+        });
+    }
+};
