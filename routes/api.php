00bb02487ae2f837fe0c4e3eaa0d08d9268322e8
absales <absales91@gmail.com>
2025-12-31 13:57:37 +0530

    ..

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\DashboardController as Dashboard;
 Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/dashboard', [Dashboard::class, 'index']);
 });
