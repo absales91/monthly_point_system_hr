6cf9f851844752844d7fb66eb7591352be3d3f56
absales <absales91@gmail.com>
2026-01-15 12:14:48 +0530

    cron fix

--- routes/console.php
+++ routes/console.php
@@ -1,26 +1,27 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 
-app()->booted(function () {
-    $schedule = app(Schedule::class);
 
-    $schedule->command('attendance:auto-out')
+   
+
+   Schedule::command('attendance:auto-out')
         ->dailyAt('23:59')
+        // ->everyMinute()
+
         ->timezone('Asia/Kolkata');
         
-    $schedule->command('attendance:mark-absent')
-        ->dailyAt('23:59')
+   Schedule::command('attendance:mark-absent')
+         ->dailyAt('23:59')
+        // ->everyMinute()
         ->timezone('Asia/Kolkata')
         ->withoutOverlapping()
         ->onOneServer();
 
      
-});
